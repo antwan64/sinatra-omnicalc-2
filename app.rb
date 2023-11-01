edc9f5100dd87b56870daf79e05d68918bfabc03
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-11-01 00:09:57 +0000

    Subtraction Passed

--- app.rb
+++ app.rb
@@ -5,33 +5,38 @@ get("/") do
   redirect("/add")
 end
 
-get("/ad") do
+get("/add") do
   erb(:add_form)
 end
 
 get("/wizard_add") do
   @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
 
   @result = @first_num + @second_num
   erb(:add_result)
 end
 
 get("/subtract") do
-  erb(:sub_form)
+  erb(:subtraction_form)
 end
 
 get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
 
-  erb(:sub_result)
+  @result = (@first_num - @second_num).abs
+
+  erb(:sub_result) 
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:multi_form)
 end
 
 get("/wizard multiply") do
+
+  
   erb(:mult_result)
 end
 
