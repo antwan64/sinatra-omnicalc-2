a1a28ef847562ac2bc820459061a55d9f4714d1e
antwan64 <146212683+antwan64@users.noreply.github.com>
2023-11-01 00:19:47 +0000

    Multiplication

--- app.rb
+++ app.rb
@@ -34,9 +34,11 @@ get("/multiply") do
   erb(:multi_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_number = params.fetch("first_num").to_f
+  @second_number = params.fetch("second_num").to_f
+  @result = @first_number * @second_number
 
-  
   erb(:mult_result)
 end
 
